10dd102c2622a8960afc84b21407a307e78081b6
Lukas-Jdk <jdklukac@gmail.com>
2025-04-26 11:35:47 +0200

    added ticket model schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique:true },
   password: { type: String, required: true },
-  bought_tickets: [{type:mongoose.Schema.Types}],
+  bought_tickets: [{type:mongoose.Schema.Types.ObjectId, ref:"Ticket"}],
   money_balance: { type: Number, required: true },
 });
-
 
 export default mongoose.model("User", userSchema)
