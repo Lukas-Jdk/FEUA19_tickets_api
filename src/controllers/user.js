9a238416ecd8dc80a2f9edeea78aac126e7d6157
Lukas-Jdk <jdklukac@gmail.com>
2025-04-28 11:40:22 +0200

    added login function with JWT auth

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,51 +5,80 @@ import User from "../models/user.js";
 
 const REGISTER_USER = async (req, res) => {
   try {
-  const { name, email, password } = req.body;
-  const id = uuidv4();
+    const { name, email, password } = req.body;
+  
 
-  // 1.patikrinu ar visi laukai yra.
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "Please fill all fields" });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const id = uuidv4();
+
+    const salt = bcryptjs.genSaltSync(10);
+    const passwordHash = bcryptjs.hashSync(password, salt);
+
+    const newUser = new User({
+      id,
+      name,
+      email,
+      password: passwordHash,
+      bought_tickets: [],
+      money_balance: 100,
+    });
+
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+    const refreshToken = jwt.sign({ id: newUser.id },process.env.JWT_REFRESH_SECRET,{
+        expiresIn: "1d",
+      });
+
+    res.status(200).json({
+      message: "Your registration was successfully",
+      token,
+      refreshToken,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error !" });
   }
-
-  const salt = bcryptjs.genSaltSync(10);
-  const passwordHash = bcryptjs.hashSync(password, salt);
-
-  const newUser = new User({
-    id,
-    name,
-    email,
-    password: passwordHash,
-    bought_tickets: [],
-    money_balance: 100,
-  });
-
-  await newUser.save();
-
-  const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
-    expiresIn: "2h",
-  });
-  const refreshToken = jwt.sign({ id: newUser.id }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: "1d",
-  });
-
-res.status(200).json({
-  message: "Your registration was succesfully",
-  token,
-  refreshToken,
-});
-} catch (error){
-  console.error(error);
-  res.status(500).json({message: "Server Error !"})
-}
 };
 
+const LOGIN_USER = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
 
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Bad email or password" });
+    }
 
+    const passwordMatch = bcryptjs.compareSync(password, user.password);
+    if (!passwordMatch) {
+      return res.status(404).json({ message: "Bad email or password" });
+    }
 
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+    const refreshToken = jwt.sign({ id: user.id },process.env.JWT_REFRESH_SECRET,{
+      expiresIn: "1d",
+      });
 
+    res.status(200).json({
+      message: "LogIn successfully ",
+      token,
+      refreshToken,
+    });
 
-export {
-  REGISTER_USER,
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error!" });
+  }
+};
+
+export { REGISTER_USER, LOGIN_USER };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   REGISTER_USER,
+  LOGIN_USER,
 } from"../controllers/user.js"
 
 const router = express.Router();
@@ -8,7 +9,7 @@ const router = express.Router();
 
 router.post("/auth/register",REGISTER_USER);
 
-
+router.post("/auth/login",LOGIN_USER);
 
 
 
